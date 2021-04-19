ba3d9b43d47d5ce7e4965a1097bf4d313f1e271c
Tykhyi Roman <73754994+NeNaO4ke@users.noreply.github.com>
2021-04-19 14:52:28 +0300

    Forecast completed

--- Frontend/www/main.js
+++ Frontend/www/main.js
@@ -3,46 +3,132 @@ let autocomplete;
 
 let map;
 var globalDate;
+var globalData;
 var globalI;
+var modI;
 
 function getNfillForecast(lat, lng) {
     var curentDate = new Date();
     console.log(new Date());
     console.log(curentDate.getHours());
-    var globalI = defineIter(curentDate.getHours());
+    globalI = defineIter(curentDate.getHours());
     console.log(globalI);
 
 
 
 
-    $.getJSON('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lng + '&cnt=' + globalI + '&lang=ua&units=metric&appid=f1c4ea79129faeebfbac6455394d7b12', function (data) {
+    $.getJSON('http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lng + '&cnt=' + globalI + '&units=metric&appid=f1c4ea79129faeebfbac6455394d7b12', function (data) {
+
+        modI = globalI % 8;
+        if (globalI == 40)
+            modI = 8;
 
 
-        var date = new Date((data["list"]["3"]["dt"]) * 1000);
-        globalDate = date;
-        var jsonDate = (date.toJSON());
-        var input = new Date(jsonDate).toString();
-        const tempSplit = input.split(" ", 4);
-        var dayOfWeek = tempSplit[0];
-        var numberOfDate = tempSplit[2];
-        var Month = tempSplit[1];
-        $( "div#day1" ).find(".day").html(dayOfWeek);
-        $( "div#day1" ).find(".date").html(numberOfDate + " " + Month);
-        console.log(dayOfWeek + numberOfDate + Month);
-      //  document.getElementById("day1").classList.remove('today');
-       // document.getElementById("day3").classList.add('today');
-        var modI = globalI % 8;
-        for(var i = 0; i < modI;i++){
-            $("#time"+(8-modI+i)*3).find(".degree").html("<img  src='images/icons/temp.svg' width=25>"+Math.round(data["list"][i]["main"]["temp"])+"<sup>o</sup>C");
-            $("#time"+(8-modI+i)*3).find(".description").html((data["list"][i]["weather"]["0"]["description"]));
-            $("#time"+(8-modI+i)*3).find(".condition").attr("src", 'http://openweathermap.org/img/wn/'+(data["list"][i]["weather"]["0"]["icon"])+'@2x.png');
-            console.log("This: "+i)
-            console.log("This: "+$("#time"+(8-modI+i)*3).find(".condition").src);
+        globalData = data;
+
+
+        var k = 0;
+        if (curentDate.getHours() >= 21)
+            k = 1;
+
+        $.getJSON('http://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lng + '&exclude=minutely,hourly,current&units=metric&appid=f1c4ea79129faeebfbac6455394d7b12', function (dataD) {
+            for (var i = 0; i < 5; i++) {
+                var date = new Date((dataD["daily"][i + k]["dt"]) * 1000);
+
+                const tempSplit = date.toString().split(" ", 4);
+                var dayOfWeek = tempSplit[0];
+                var numberOfDate = tempSplit[2];
+                var Month = tempSplit[1];
+                $("#day" + (i + 1)).find(".day").html(dayOfWeek);
+                $("#day" + (i + 1)).find(".date").html(numberOfDate + " " + Month);
+                $("#day" + (i + 1)).find(".location").html(data["city"]["name"]);
+                $("#day" + (i + 1)).find(".num").html(Math.round(dataD["daily"][i + k]["temp"]["day"]) + "<sup>o</sup>C");
+                $("#day" + (i + 1)).find(".condition").attr("src", 'http://openweathermap.org/img/wn/' + (dataD["daily"][i + k]["weather"]["0"]["icon"]) + '@2x.png');
+                $("#day" + (i + 1)).find(".pop").html('<img src="images/icon-umberella.png" alt="">' + Math.round(100 * (dataD["daily"][i + k]["pop"])) + "%");
+                $("#day" + (i + 1)).find(".wind").html('<img src="images/icon-wind.png" alt="">' + (dataD["daily"][i + k]["wind_speed"]) + "m/s");
+                console.log(dayOfWeek + numberOfDate + Month);
+                console.log("Date: " + date.toString());
+
+            };
+
+        });
+
+        $("#day1").trigger("click");
+        var x = document.querySelector(".forecast-table");
+        const style = getComputedStyle(x).display;
+
+        console.log(style);
+        if (style == "none") {
+            $(".forecast-table").slideToggle("slow", function () {
+                // Animation complete.
+            });
+            $(".time-for").slideToggle("slow", function () {
+                // Animation complete.
+            });
         }
-       
+
+
+
     });
 
 }
+
+
+
+$(".forecast").click(function () {
+    var ID = $(this).attr("id");
+    var prev = $(".forecast-container").find(".today").attr("id");
+
+    $("#" + prev).removeClass("today");
+    $("#" + prev).find("span").addClass("invisible");
+    $("#" + prev).find(".day").addClass("d-none");
+    $("#" + prev).find(".location").addClass("invisible");
+    $(this).find("span").removeClass("invisible");
+    $(this).find(".day").removeClass("d-none");
+    $(this).find(".location").removeClass("invisible");
+    $(this).addClass("today");
+    const tempSplit = (ID).split("day", 2);
+    var numberOfDay = tempSplit[1];
+
+
+
+    modI = globalI % 8;
+    if (globalI == 40)
+        modI = 8;
+
+    globalI = defineIter(new Date().getHours());
+    if (numberOfDay == 1)
+        for (var i = 0; i < modI; i++) {
+            $("#time" + (8 - modI + i) * 3).find(".temp").html("<img  src='images/icons/temp.svg' width=25>" + Math.round(globalData["list"][i]["main"]["temp"]) + "<sup>o</sup>C");
+            $("#time" + (8 - modI + i) * 3).find(".pop").html("<img  src='images/icon-umberella.png' width=25>" + +Math.round(100 * (globalData["list"][i]["pop"])) + "%");
+            $("#time" + (8 - modI + i) * 3).find(".wind").html('<img src="images/icon-wind.png" alt="">' + (globalData["list"][i]["wind"]["speed"]) + "m/s");
+            $("#time" + (8 - modI + i) * 3).find(".description").html((globalData["list"][i]["weather"]["0"]["description"]));
+            $("#time" + (8 - modI + i) * 3).find(".condition").attr("src", 'http://openweathermap.org/img/wn/' + (globalData["list"][i]["weather"]["0"]["icon"]) + '@2x.png');
+            console.log("This: " + i)
+
+        }
+    else {
+        $("#time0, #time3, #time6, #time9, #time12, #time15, #time18, #time21").removeClass('d-none');
+
+        for (var i = modI + 8 * (numberOfDay - 2); i < modI + 8 * (numberOfDay - 1); i++) {
+            $("#time" + (i - modI - 8 * (numberOfDay - 2)) * 3).find(".temp").html("<img  src='images/icons/temp.svg' width=25>" + Math.round(globalData["list"][i]["main"]["temp"]) + "<sup>o</sup>C");
+            $("#time" + (i - modI - 8 * (numberOfDay - 2)) * 3).find(".pop").html("<img  src='images/icon-umberella.png' width=25>" + +Math.round(100 * (globalData["list"][i]["pop"])) + "%");
+            $("#time" + (i - modI - 8 * (numberOfDay - 2)) * 3).find(".wind").html('<img src="images/icon-wind.png" alt="">' + (globalData["list"][i]["wind"]["speed"]) + "m/s");
+            $("#time" + (i - modI - 8 * (numberOfDay - 2)) * 3).find(".description").html((globalData["list"][i]["weather"]["0"]["description"]));
+            $("#time" + (i - modI - 8 * (numberOfDay - 2)) * 3).find(".condition").attr("src", 'http://openweathermap.org/img/wn/' + (globalData["list"][i]["weather"]["0"]["icon"]) + '@2x.png');
+            console.log("This: " + i)
+
+        }
+
+    }
+});
+document.getElementById("time0").classList.remove('d-none');
+document.getElementById("time3").classList.remove('d-none');
+document.getElementById("time6").classList.remove('d-none');
+document.getElementById("time9").classList.remove('d-none');
+document.getElementById("time12").classList.remove('d-none');
+document.getElementById("time15").classList.remove('d-none');
+document.getElementById("time18").classList.remove('d-none');
 
 function defineIter(hours) {
     if (hours < 3) {
